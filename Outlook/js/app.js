6e4fa204367cf7f2dc82c6a87fc5cda4cd9843ab
Ohlamesaint <mkop9456@gmail.com>
2020-03-26 00:57:35 +0800

    commit msg

--- Outlook/js/app.js
+++
@@ -1,5 +0,0 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("../sw.js")
-        .then((reg)=>console.log("service worker registered", reg))
-        .catch((err)=>console.log(new Error("service worker not registered"), err))
-}
