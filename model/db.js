13676f5e555e8cb1f8cbb14d28ff9a0d6a47fef0
Ohlamesaint <mkop9456@gmail.com>
2020-03-27 00:57:30 +0800

    commit msg

--- model/db.js
+++ model/db.js
@@ -3,14 +3,14 @@ var url_db = process.env.MONGODB_URI || "mongodb://localhost:27017/"        //da
 var mongoClient = require("mongodb").MongoClient;
 
 function getAccount(callback){
-    mongoClient.connect(url_db, function(err, db){
+    mongoClient.connect(url_db, function(err, database){
         if(err){
             console.log("error to connect with db")
             return;
         }
         console.log("connect success");
-
-        db.db.collection("shang").find({}).toArray(function(err, arr){
+        const myDB = database.db("shang");
+        myDB.collection("shang").find({}).toArray(function(err, arr){
             callback(arr);
             db.close();
         })
