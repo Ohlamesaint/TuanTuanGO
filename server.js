a36e795ea8b11e5c9ad68278c412518da161d5f3
Ohlamesaint <mkop9456@gmail.com>
2020-04-04 23:09:26 +0800

    commit msg

--- server.js
+++ server.js
@@ -75,7 +75,8 @@ app.get('/profile', function(req, res, next){
     }else{
         UserProfile.checkAccount(req.session.username, (result)=>{
             console.log(result);
-            var headPasteBuf = (result.headPaste.buffer).toString('utf8');
+            var headPasteBuf = JSON.parse((result.headPaste.buffer).toString());
+            console.log(headPasteBuf);
             var headPasteJSON = JSON.stringify(headPasteBuf);
             fs.readFileSync(result.headPaste, 'utf8', (err, data)=>{
                 if(err){
