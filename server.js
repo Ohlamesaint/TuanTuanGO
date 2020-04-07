146904cab094beba54241df31c9218f26295fb49
Ohlamesaint <mkop9456@gmail.com>
2020-04-07 12:40:58 +0800

    commit msg

--- server.js
+++ server.js
@@ -102,16 +102,11 @@ app.get('/profile', function(req, res, next){
 })
 
 app.get('/signOut', function(req, res, next){
-    // 備註：這裡用的 session-file-store 在destroy 方法裡，並沒有銷燬cookie
-    // 所以客戶端的 cookie 還是存在，導致的問題 --> 退出登陸後，服務端檢測到cookie
-    // 然後去查詢對應的 session 檔案，報錯
-    // session-file-store 本身的bug  
     req.session.destroy(function(err) {
         if(err){
             res.json({ret_code: 2, ret_msg: '退出登入失敗'});
             return;
         }
-        // req.session.loginUser = null;
         res.clearCookie("sessid");
         res.send("登出成功!");
     });
@@ -186,6 +181,13 @@ app.post("/registration", (req, res, next)=>{
             }
         })
     })
+})
+
+app.get(/(products)\/:check$/, (req, res, next)=>{
+    if(/[\d]{6}/.test(check)){
+        var productNum = check;
+        console.log(productNum);
+    }
 })
 
 // app.get('/static/:anything', (req, res, next)=>{
