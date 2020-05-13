eb98fa12d05fff4bd53658d81a9146c6edaa089a
Ohlamesaint <mkop9456@gmail.com>
2020-05-14 03:34:01 +0800

    [PWA] Notification

--- server.js
+++ server.js
@@ -315,13 +315,13 @@ const PushSubsciption = (endpoint, p256dh, auth) => {
 const payload = JSON.stringify({title: "tuantuanGO", content: "your tuanGO is ready!"});
 const options = {
     vapidDetails : {
-        subject: 'mailto:mkop9456@gmail.com',
-        publicKey: "BGxHf6ZQkHVoIdROO4Fir61eouPlqUp3IzxsV4ud10FeXgS5vvG9q3Gw5J7lsp2XHnF_49aJ9RxWNV99_TD9--8",
-        privateKey: 'Dif3UmuTtyX9iwlvQYCaiIKXBgsk-pK-5TQS_QAosFU'
+        
     }
 }
 
-
+const subject = 'mailto:mkop9456@gmail.com',
+const publicKey = "BGxHf6ZQkHVoIdROO4Fir61eouPlqUp3IzxsV4ud10FeXgS5vvG9q3Gw5J7lsp2XHnF_49aJ9RxWNV99_TD9--8",
+const privateKey = 'Dif3UmuTtyX9iwlvQYCaiIKXBgsk-pK-5TQS_QAosFU'
 let pushConfig = {
     endpoint: "https://fcm.googleapis.com/fcm/send/cTSrRcs9Duk:APA91bHf14z6XSb8YOXgjsvrB6CE7OGEOrrwCwHWUsnmvwQRpTIONQkAZkE-dQVAaYiTHHuAlu9jeJplpvQ8fgcNg-0PlNsiHH6kAnXM8Zty2Dm5cG91fzFPpW_vNIifNTl7r5g038Rf",
     keys: {
@@ -339,7 +339,8 @@ app.post('/subscribe', (req, res) => {
         auth: data.keys.auth
     })
     newSubscription.save();
-    webpush.sendNotification(data, payload).catch((err) => console.log(err));
+    webpush.setVapidDetails('mailto:mkop9456@gmail.com', publicKey, privateKey);
+    webpush.sendNotification(data, payload).catch((err) => console.log('something went wrong => webpush', err));
 })
 
 app.post('/mainPageProducts', (req, res, next)=>{
